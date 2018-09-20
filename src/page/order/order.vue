b81398bb8439b0c007b4efce64e66f515cb85118
Meteoors <229281264@qq.com>
2018-09-20 21:59:28 +0800

    update profile

---
+++ src/page/order/order.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id='order'>
+        <head-top go-back='true' head-title='订单列表'></head-top>
+
+        <ul class='order_ul'>
+            <li class='order_li' v-for='(order, index) in orderlist' :key='index'>
+                <div class='avator'>
+                    <img src="imgBaseUrl + order.restaurant_umage_url">
+                </div>
+                <div class='detail'>
+                    <div class='top'>
+                        <div class='title'>
+                            <span class='name'>效果演示</span>
+                            <span class='pay'>等待支付</span>
+                        </div>
+                        <div class='date'>2018-09-20 17:58</div>
+                    </div>
+                    <div class='middle'>
+                        
+                    </div>
+                </div>
+            </li>
+        </ul>
+
+        <foot-guide parent='order'></foot-guide>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style lang='scss'>
+
+</style>
--- src/page/profile/profile.vue
+++ src/page/profile/profile.vue
@@ -2,41 +2,41 @@
     <div id='profile'>
         <head-top go-back='true' head-title='我的'></head-top>
 
-        <div class='login'>
-            <router-link class='login_link' to='/login'>
-                <div class='avatar'>
-                    <svg class='profile_photo'>
-                        <use xlink:href='#avator-dafault'></use>
+        <router-link class='login_link' :to='userInfo ? "/info":"/login"' tag='div'>
+            <div class='avatar'>
+                <img :src='imgBaseUrl + userInfo.avatar' v-if='userInfo.avatar' class='profile_img'>
+                <svg class='profile_photo' v-else fill='#fff'>
+                    <use xlink:href='#avatar-default'></use>
+                </svg>
+            </div>
+            <div class='userinfo'>
+                <p class='title'>{{username}}</p>
+                <p class='phone'>
+                    <svg class='mobile'>
+                        <use xlink:href='#mobile'></use>
                     </svg>
-                </div>
-                <div class='userinfo'>
-                    <p class='title'>登录/注册</p>
-                    <p class='phone'>
-                        <svg class='mobile'>
-                            <use xlink:href='#mobile'></use>
-                        </svg>
-                        <span class='num'>暂无绑定手机号</span>
-                    </p>
-                </div>
-                <div class='icon'>
-                    <svg class='arrow_right'>
-                        <use xlink:href='#arrow-right'></use>
-                    </svg>
-                </div>
-            </router-link>
-        </div>
+                    <span class='num'>{{mobile}}</span>
+                </p>
+            </div>
+            <div class='icon'>
+                <svg class='arrow_right'>
+                    <use xlink:href='#arrow-right'></use>
+                </svg>
+            </div>
+        </router-link>
 
-        <ul class='info'>
+
+        <ul class='info clear'>
             <router-link to='/balance' tag='li' class='info_li'>
-                <p class='top'><span>0.00</span>元</p>
+                <p class='top'><span>{{balance.toFixed(2)}}</span>元</p>
                 <p class='bottom'>我的余额</p>
             </router-link>
             <router-link to='/benefit' tag='li' class='info_li'>
-                <p class='top'><span>0</span>个</p>
+                <p class='top'><span>{{count}}</span>个</p>
                 <p class='bottom'>我的优惠</p>
             </router-link>
             <router-link to='/points' tag='li' class='info_li'>
-                <p class='top'><span>0</span>分</p>
+                <p class='top'><span>{{point}}</span>分</p>
                 <p class='bottom'>我的积分</p>
             </router-link>
         </ul>
@@ -57,7 +57,7 @@
 
             <!-- 积分商城 -->
             <a href='https://home.m.duiba.com.cn/#/chome/index' class='detail'>
-                <svg fill="#4aa5f0">
+                <svg fill="#fc7b53">
                     <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#point"></use>
                 </svg> 
                 <span>积分商城</span>
@@ -70,10 +70,10 @@
 
             <!-- 饿了么会员卡 -->
             <router-link to='/vipcard' class='detail'>
-                <svg fill="#4aa5f0">
+                <svg fill="#ffc636">
                     <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#vip"></use>
                 </svg> 
-                <span>饿了么会员卡</span>
+                <span class='border_none'>饿了么会员卡</span>
                 <div class='icon'>
                     <svg fill="#bbb">
                         <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-right"></use>
@@ -98,10 +98,10 @@
 
             <!-- 下载饿了么APP -->
             <router-link to='/download' class='detail'>
-                <svg fill="#4aa5f0">
+                <svg fill="#3cabff">
                     <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#download"></use>
                 </svg> 
-                <span>下载饿了么APP</span>
+                <span class='border_none'>下载饿了么APP</span>
                 <div class='icon'>
                     <svg fill="#bbb">
                         <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-right"></use>
@@ -115,11 +115,199 @@
 </template>
 
 <script>
+    import {imgBaseUrl} from '../../config/env'
+    import { mapState } from 'vuex';
+    import headTop from '../../components/head/header'
+    import footGuide from '../../components/foot/footer'
+
     export default {
-        
+        data() {
+            return {
+                imgBaseUrl,
+                username: '登录/注册',
+                mobile: '暂无绑定手机号',
+                balance: 0,
+                count: 0,
+                point: 0,
+            }
+        },
+        computed: {
+            ...mapState([
+                'userInfo'
+            ])
+        },
+        created() {
+            this.init();
+        },
+        components: {
+            headTop,
+            footGuide
+        },
+        methods: {
+            init () {
+                if(typeof this.userInfo.id === 'number'){
+                    this.username = this.userInfo.username;
+                    this.mobile = this.userInfo.mobile||'暂无绑定手机号';
+                    this.balance = this.userInfo.balance;
+                    this.count = this.userInfo.gift_amount;
+                    this.point = this.userInfo.point;
+                }
+            }
+        },
+        watch: {
+            userInfo: function() {
+                this.init();
+            }
+        }
     }
 </script>
 
-<style lang='scss'>
+<style lang='scss' scoped>
+    @import '../../style/mixin';
 
+    #profile{
+        p, span{
+            font-family: Helvetica Neue,Tahoma,Arial;
+        }
+    }
+
+    #profile{
+        padding-top: 1.9rem;
+        .login_link{
+            padding: .65rem .6rem;
+            display: flex;
+            align-items: center;
+            background: $blue;
+            .avatar{
+                width: 2.5rem;
+                height: 2.5rem;
+                border-radius: 50%;
+                overflow: hidden;
+                .profile_img,.profile_photo{
+                    width: 100%;
+                    height: 100%;
+                }
+            }
+            .userinfo{
+                flex: 1;
+                margin-left: .48rem;
+                .title{
+                    color: #fff;
+                    font-size: .8rem;
+                    font-weight: 700;
+                    line-height: .8rem;
+                }
+                .phone{
+                    display: flex;
+                    align-items: center;
+                    padding-top: .3rem;
+                    .mobile{
+                        width: .5rem;
+                        height: .75rem;
+                        fill: #fff;
+                        line-height: .75rem;
+                        display: inline-block;
+                        margin-right: .2rem;
+                    }
+                    .num{
+                        color: #fff;
+                        font-size: .6rem;
+                        line-height: .6rem;
+                    }
+                }
+            }
+            .icon{
+                height: 1rem;
+                width: .47rem;
+                .arrow_right{
+                    fill: #fff;
+                    width: 100%;
+                    height: 100%;
+                }
+            }
+        }
+
+        .info{
+            background: #fff;
+            li{
+                float: left;
+                width: 33%;
+                border-right: 1px solid #f1f1f1;
+                .top{
+                    padding: .85rem 0 .45rem;
+                    font-size: .6rem;
+                    color: #333;
+                    text-align: center;
+                    span{
+                        color: #f90;
+                        font-size: 1.2rem;
+                        font-weight: 700;
+                        line-height: 1rem;
+                        font-family: Helvetica Neue,Tahoma;
+                    }
+                }
+                .bottom{
+                    padding-bottom: .45rem;
+                    font-size: .6rem;
+                    color: #666;
+                    text-align: center;
+                }
+            }
+            li:nth-child(2){
+                .top{
+                    span{
+                        color: #ff5f3e;
+                    }
+                }
+            }
+            li:nth-child(3){
+                border: none;
+                .top{
+                    span{
+                        color: #6ac20b;
+                    }
+                }
+            }
+        }
+
+        .service{
+            margin-top: .4rem;
+            background: #fff;
+            .detail{
+                display: flex;
+                padding-left: .8rem;
+                position: relative;
+                align-items: center;
+                svg{
+                    height: .7rem;
+                    width: .7rem;
+                    margin-right: .25rem;
+                }
+                span{
+                    display: inline-block;
+                    width: 100%;
+                    border-bottom: 1px solid #f1f1f1;
+                    line-height: 1.8rem;
+                    font-size: .7rem;
+                    color: #333;
+                }
+                .border_none{
+                    border: none;
+                }
+                .icon{
+                    @include ct;                    
+                    width: .47rem;
+                    height: .47rem;
+                    right: .2rem;
+                    svg{
+                        width: 100%;
+                        height: 100%;
+                        position: absolute;
+                        left: 0;
+                        top: 0;
+                    }
+                }
+            }
+        }
+    }
 </style>
